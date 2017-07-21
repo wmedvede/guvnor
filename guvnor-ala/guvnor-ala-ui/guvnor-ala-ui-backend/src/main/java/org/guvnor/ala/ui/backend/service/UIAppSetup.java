66fa6383a625af5b622ba28debc4bb355ac3daa8
Walter Medvedeo <wmedvedeo@gmail.com>
2017-07-21 14:33:11 +0200

    Openshift provider integration I

--- guvnor-ala/guvnor-ala-ui/guvnor-ala-ui-backend/src/main/java/org/guvnor/ala/ui/backend/service/UIAppSetup.java
+++ guvnor-ala/guvnor-ala-ui/guvnor-ala-ui-backend/src/main/java/org/guvnor/ala/ui/backend/service/UIAppSetup.java
@@ -16,8 +16,12 @@
 
 package org.guvnor.ala.ui.backend.service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
@@ -33,19 +37,34 @@ import org.guvnor.ala.config.ProjectConfig;
 import org.guvnor.ala.config.ProviderConfig;
 import org.guvnor.ala.config.RuntimeConfig;
 import org.guvnor.ala.config.SourceConfig;
+import org.guvnor.ala.openshift.config.OpenShiftProviderConfig;
+import org.guvnor.ala.openshift.config.impl.ContextAwareOpenShiftRuntimeExecConfig;
+import org.guvnor.ala.openshift.config.impl.OpenShiftProviderConfigImpl;
+import org.guvnor.ala.openshift.model.OpenShiftProvider;
+import org.guvnor.ala.openshift.model.OpenShiftProviderType;
 import org.guvnor.ala.pipeline.Input;
 import org.guvnor.ala.pipeline.Pipeline;
 import org.guvnor.ala.pipeline.PipelineFactory;
 import org.guvnor.ala.pipeline.Stage;
 import org.guvnor.ala.registry.PipelineRegistry;
 import org.guvnor.ala.services.api.backend.PipelineConfigImpl;
+import org.guvnor.ala.services.api.backend.PipelineServiceBackend;
 import org.guvnor.ala.source.git.config.GitConfig;
+import org.guvnor.ala.ui.model.ProviderConfiguration;
+import org.guvnor.ala.ui.model.ProviderType;
+import org.guvnor.ala.ui.model.ProviderTypeKey;
+import org.guvnor.ala.ui.service.ProviderService;
 import org.guvnor.ala.wildfly.config.WildflyProviderConfig;
 import org.guvnor.ala.wildfly.config.impl.ContextAwareWildflyRuntimeExecConfig;
 import org.guvnor.ala.wildfly.model.WildflyProviderType;
 import org.uberfire.commons.services.cdi.Startup;
 import org.uberfire.commons.services.cdi.StartupType;
 
+import static org.guvnor.ala.config.ProviderConfig.PROVIDER_NAME;
+import static org.guvnor.ala.openshift.config.OpenShiftProperty.KUBERNETES_AUTH_BASIC_PASSWORD;
+import static org.guvnor.ala.openshift.config.OpenShiftProperty.KUBERNETES_AUTH_BASIC_USERNAME;
+import static org.guvnor.ala.openshift.config.OpenShiftProperty.KUBERNETES_MASTER;
+import static org.guvnor.ala.openshift.config.OpenShiftProperty.KUBERNETES_NAMESPACE;
 import static org.guvnor.ala.pipeline.StageUtil.config;
 
 /**
@@ -58,18 +77,28 @@ public class UIAppSetup {
 
     private PipelineRegistry pipelineRegistry;
 
+    private ProviderService providerService;
+
+    PipelineServiceBackend pipelineServiceBackend;
+
     public UIAppSetup() {
         //Empty constructor for Weld proxying
     }
 
     @Inject
-    public UIAppSetup(PipelineRegistry pipelineRegistry) {
+    public UIAppSetup(PipelineRegistry pipelineRegistry,
+                      ProviderService providerService,
+                      PipelineServiceBackend pipelineServiceBackend) {
         this.pipelineRegistry = pipelineRegistry;
+        this.providerService = providerService;
+        this.pipelineServiceBackend = pipelineServiceBackend;
     }
 
     @PostConstruct
     private void init() {
         initPipelines();
+        initOpenshiftProvider();
+        initOpenshiftPipeline();
     }
 
     protected void initPipelines() {
@@ -172,5 +201,78 @@ public class UIAppSetup {
                                           WildflyProviderType.instance());
         pipelineRegistry.registerPipeline(pipeline2,
                                           WildflyProviderType.instance());
+    }
+
+    private void initOpenshiftPipeline() {
+        final Stage<Input, ProviderConfig> providerConfig =
+                config("OpenShift Provider Config",
+                       (s) -> new OpenShiftProviderConfig() {
+                       });
+
+        final Stage<ProviderConfig, RuntimeConfig> runtimeExec =
+                config("OpenShift Runtime Config",
+                       (s) -> new ContextAwareOpenShiftRuntimeExecConfig());
+
+        final Pipeline pipe = PipelineFactory
+                .startFrom(providerConfig)
+                .andThen(runtimeExec)
+                .buildAs("openshift pipeline");
+
+        pipelineRegistry.registerPipeline(pipe,
+                                          OpenShiftProviderType.instance());
+    }
+
+    private void initOpenshiftProvider() {
+
+        String namespace = createNamespace();
+
+        OpenShiftProviderConfigImpl providerConfig = new OpenShiftProviderConfigImpl();
+        providerConfig.setName("openshift-provider-test");
+        providerConfig.setKubernetesMaster("https://ce-os-rhel-master.usersys.redhat.com:8443");
+        providerConfig.setKubernetesAuthBasicUsername("admin");
+        providerConfig.setKubernetesAuthBasicPassword("admin");
+        providerConfig.setKubernetesNamespace(namespace);
+
+        final Map<String, Object> values = new HashMap<>();
+        values.put(KUBERNETES_MASTER.inputKey(),
+                   "https://ce-os-rhel-master.usersys.redhat.com:8443");
+        values.put(KUBERNETES_NAMESPACE.inputKey(),
+                   namespace);
+        values.put(KUBERNETES_AUTH_BASIC_USERNAME.inputKey(),
+                   "admin");
+        values.put(KUBERNETES_AUTH_BASIC_PASSWORD.inputKey(),
+                   "admin");
+
+        ProviderConfiguration providerConfiguration = new ProviderConfiguration("openshift-provider-test",
+                                                                                values);
+
+        providerService.createProvider(new ProviderType(new ProviderTypeKey(OpenShiftProviderType.instance().getProviderTypeName(),
+                                                                            OpenShiftProviderType.instance().getVersion()),
+                                                        "OpenShift"),
+                                       providerConfiguration);
+
+        /*
+        Taken from OpenShiftRuntimeExecutorTest
+        put(KUBERNETES_MASTER.inputKey(), "https://ce-os-rhel-master.usersys.redhat.com:8443");
+        put(KUBERNETES_AUTH_BASIC_USERNAME.inputKey(), "admin");
+        put(KUBERNETES_AUTH_BASIC_PASSWORD.inputKey(), "admin");
+            / unnecessary for this test
+            put(RESOURCE_TEMPLATE_NAME.inputKey(), "bpmsuite70-execserv");
+            put(RESOURCE_TEMPLATE_PARAM_DELIMITER.inputKey(), ",");
+            put(RESOURCE_TEMPLATE_PARAM_ASSIGNER.inputKey(), "=");
+             /
+        put(KUBERNETES_NAMESPACE.inputKey(), namespace);
+        */
+    }
+
+    private String createNamespace() {
+        return new StringBuilder()
+                .append("guvnor-ala-")
+                .append(System.getProperty("user.name",
+                                           "anonymous").replaceAll("[^A-Za-z0-9]",
+                                                                   "-"))
+                .append("-test-")
+                .append(new SimpleDateFormat("YYYYMMddHHmmss").format(new Date()))
+                .toString();
     }
 }
